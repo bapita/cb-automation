1d63a5ad229402173a1f2cc15111a2ee032734bd
bapita <bapitaroy14@gmail.com>
2022-06-11 18:15:55 +0530

    created test scripts for login

--- src/main/java/com/cb/qa/pages/LoginPage.java
+++ src/main/java/com/cb/qa/pages/LoginPage.java
@@ -1,12 +1,38 @@
 package com.cb.qa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import com.cb.qa.base.TestBase;
 
 public class LoginPage extends TestBase {
 
-	public static void main(String[] args) {
-		
+	// Creating object repository
+	@FindBy(name = "email")
+	WebElement email;
 
+	@FindBy(name = "password")
+	WebElement password;
+
+	@FindBy(xpath = "@button[type='submit']")
+	WebElement loginBtn;
+
+	// Initializing the Page Objects with this(current class objects)
+	public LoginPage() {
+
+		PageFactory.initElements(driver, this);
 	}
+	
+	// login method
+		public DashboardPage login(String en, String pwd) { 
+			 email.sendKeys(en); 
+			 password.sendKeys(pwd);
+			 loginBtn.click(); 
+			 
+			 // after clicking on login button, it is moving to home page. So DashboardPage is the return type of login function
+			 return new DashboardPage(); 
+			// returning HomePage class object. Instead of void return type, the DashboardPage class is to be mentioned
+		}
 
 }
--- src/test/java/com/cb/qa/testcases/LoginpageTest.java
+++ src/test/java/com/cb/qa/testcases/LoginpageTest.java
@@ -1,9 +1,23 @@
 package com.cb.qa.testcases;
 
 import com.cb.qa.base.TestBase;
+import com.cb.qa.pages.DashboardPage;
+import com.cb.qa.pages.LoginPage;
 
 public class LoginpageTest extends TestBase {
 	
+	// Objects of required pages for testing
+	LoginPage loginpage;
+	DashboardPage dashboardpage;
 	
-
+	// Defining constructor of the class 
+	public LoginpageTest() {
+		super();
+	}
+	
+	@BeforeMethod
+	public void setUp() {
+		initialization();
+	}
+	
 }
